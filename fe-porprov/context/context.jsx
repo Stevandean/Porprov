c2b421aa6dccce2cefff71e15f8eed9111f164dd
RazaqFarhanR <aljawadfarhan@gmail.com>
2023-01-27 23:49:02 +0700

    upd: timer

--- fe-porprov/context/context.jsx
+++ fe-porprov/context/context.jsx
@@ -1,2 +1,14 @@
-import React from 'react'
-export const globalState = React.createContext();
+import React, {createContext, useState} from 'react'
+export const globalState = createContext();
+
+function Context({ children }) {
+    const [duration,setDuration]= useState(0)
+    const [running,setRunning]= useState(false)
+  
+    return (
+      <globalState.Provider value={{ duration, setDuration, running, setRunning }}>
+        {children}
+      </globalState.Provider>
+    );
+  }
+export default Context;
